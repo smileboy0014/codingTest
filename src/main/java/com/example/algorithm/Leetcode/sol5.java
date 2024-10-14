1fd89b361fa170d0bdda268f4c952e0f959953c4
gt.park <smileboy0014@gmail.com>
2024-10-14 18:42:45 +0900

    feat: sol leetcode 5

---
+++ src/main/java/com/example/algorithm/Leetcode/sol5.java
@@ -0,0 +1,38 @@
+package com.example.algorithm.Leetcode;
+
+public class sol5 {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("abbaca"));
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            String odd = expandFromCenter(s, i, i); //홀수인 경우
+            String even = expandFromCenter(s, i, i + 1); //짝수인 경우
+
+            if (odd.length() > maxStr.length()) {
+                maxStr = odd;
+            }
+            if (even.length() > maxStr.length()) {
+                maxStr = even;
+            }
+        }
+
+        return maxStr;
+    }
+
+    private static String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+}
