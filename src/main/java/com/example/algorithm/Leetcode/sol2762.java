a1b2ecaf089c2ad650c412e6e349203c367c88df
gt.park <smileboy0014@gmail.com>
2024-10-21 00:27:09 +0900

    sol leetcode 2762

--- src/main/java/com/example/algorithm/Leetcode/sol2762.java
+++ src/main/java/com/example/algorithm/Leetcode/sol2762.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class sol2762 {
     public static void main(String[] args) {
-        System.out.println(continuousSubarrays(new int[]{5,4,2,4}));
+        System.out.println(continuousSubarrays(new int[]{5, 4, 2, 4}));
     }
 
     public static long continuousSubarrays(int[] nums) {
@@ -13,24 +13,26 @@ public class sol2762 {
         long ans = 0L;
         Deque<Integer> max = new LinkedList<>();
         Deque<Integer> min = new LinkedList<>();
-        int j=0;
-        for(int i=0; i<n; i++) {
-            while(!max.isEmpty() && nums[max.peekLast()]<nums[i]) {
+        int start = 0;
+        for (int end = 0; end < n; end++) {
+            while (!max.isEmpty() && nums[max.peekLast()] < nums[end]) { //최댓값 유지를 위해
                 max.pollLast();
             }
-            max.offerLast(i);
-            while(!min.isEmpty() && nums[min.peekLast()]>nums[i]) {
+            max.offerLast(end);
+
+            while (!min.isEmpty() && nums[min.peekLast()] > nums[end]) { //최솟값을 유지하기 위해
                 min.pollLast();
             }
-            min.offerLast(i);
+            min.offerLast(end);
 
-            while(!max.isEmpty() && !min.isEmpty() && nums[max.peekFirst()] - nums[min.peekFirst()] > 2) {
-                if(max.peekFirst() < min.peekFirst())
-                    j = 1+max.pollFirst();
-                else
-                    j = 1+min.pollFirst();
+            while (!max.isEmpty() && !min.isEmpty() && nums[max.peekFirst()] - nums[min.peekFirst()] > 2) {
+                if (max.peekFirst() < min.peekFirst()) {
+                    start = 1 + max.pollFirst();
+                } else {
+                    start = 1 + min.pollFirst();
+                }
             }
-            ans += i - j + 1;
+            ans += end - start + 1;
         }
         return ans;
     }
